e34c3fddc6a9fd937475186919f4af3c8904aa2f
edgarefrent <42181839+edgarefrent@users.noreply.github.com>
2018-08-31 10:48:54 -0500

    Add files via upload

---
+++ numero_mayor.py
@@ -0,0 +1,14 @@
+a=input("Dame un numero: ")
+b=input("Dame otro numero: ")
+c=input("Dame un tercer numero: ")
+d=input("Dame un cuarto numero: ")
+
+if a>b and a>c and a>d :
+	print("El primer numero es el mas grande, y es:",a,)
+if b>a and b>c and b>d :
+	print("El segundo numero es el mas grande, y es:",b,)
+if c>a and c>b and c>d :
+	print("El tercer numero es el mas grande, y es:",c,)
+if d>a and d>b and d>c :
+	print("El cuarto numero es el mas grande, y es:",d,)
+
